596dfd4176a00e89ef45de517cdea5e2b7d92e59
Chan Woo Park <14cpark@gmail.com>
2017-10-31 13:42:32 +0000

    moved database interaction to BlogMainController remove sample blogpost clutter from index

--- public/app.js
+++ public/app.js
@@ -10,8 +10,27 @@
             console.log("PortfolioController app.js init()");
         }
         
+        init();
+    });
+    
+    /**
+     * Blog-related controllers
+     */
+    app.controller("BlogMainController", function($scope, $http) {
+        
+        $scope.readMore = function() {
+            alert("readMore() clicked");
+            // $state.go("/tab/newpost");
+        }
+        
+        function init() {
+            console.log("BlogMainController app.js init()");
+            createJson();
+            loadPosts();
+        }
+        
         function createJson() {
-            var text = '{ "title" : "t1", "body" : "b1"}';
+            var text = '{ "title" : "t1", "body" : "<h1>My First Heading</h1> <p>My first paragraph.</p>" }';
             var obj = JSON.parse(text);
             console.log("created JSON obj @ createJson()");
             console.log(obj);
@@ -20,15 +39,24 @@
                 .then(console.log("post request made to /api/blogposts @ createJson()"));
         }
         
+        function getAllPosts() { // retrieve all latest blogposts in database and render on main page
+            $http
+                .get("/api/blogposts") // restful syntax interprets this as all blogposts // get() returns client-side promise object which expects response from server
+                .then(
+                    function (allBlogPosts) {
+                        $scope.blogpostlist = allBlogPosts.data; // render post array (data portion of response) at {{blogpostlist}}
+                    }
+                );
+        }
+        
+        /**
+         * Load up to 3 recent blogposts on blog main
+         */
+        function loadPosts() {
+            
+        }
+        
         init();
-        createJson();
-    });
-    
-    /**
-     * Blog-related controllers
-     */
-    app.controller("BlogMainController", function($scope) {
-        console.log("BlogMainController app.js 2");
     });
     
     // app.controller("BlogAboutController", BlogAboutController);

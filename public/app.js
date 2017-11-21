32e6b914b8917c65aec3ab17ad4933134c25ee7b
Chan Woo Park <14cpark@gmail.com>
2017-11-21 23:40:17 +0000

    fix major choke with express static source for blog directory which prevented partials from showing (removed first param '/blog') route single blog url to /blog/single setup more links here n there

--- public/app.js
+++ public/app.js
@@ -3,9 +3,13 @@
     
     app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
         $routeProvider
-            .when('/', {
+            .when('/blog', {
                 templateUrl: 'partials/blogMain.html',
                 controller: 'blogMainController'
+            })
+            .when('/blog/single', {
+                templateUrl: 'partials/single.html',
+                controller: 'singleController'
             });
         
         $locationProvider.html5Mode({enabled: true, requireBase: false});

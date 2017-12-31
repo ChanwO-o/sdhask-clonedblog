40a565367ca1d9c70fe87eb420d96e5794fcf07c
Chan Woo Park <14cpark@gmail.com>
2017-12-31 08:50:50 +0000

    render blogpost content on single.html using ng-bind-html + ngSanitize

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-    var app = angular.module("WebsiteApp", ['ngRoute'] );
+    var app = angular.module("WebsiteApp", ['ngRoute', 'ngSanitize'] );
     
     app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
         $routeProvider
--- public/blog/js/controllers/singleController.js
+++ public/blog/js/controllers/singleController.js
@@ -3,6 +3,7 @@ app.controller("singleController", function($scope, $http, sharedata) {
     function init() {
         var post = sharedata.getViewBlog();
         $scope.singlepost = post;
+        $scope.singlepostbody = post.body;
         
         console.log(post.posted);
     }

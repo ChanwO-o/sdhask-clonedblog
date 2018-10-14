26739c6d05fad90de7ae1ca2c1c489fb86a032e0
Chan Woo Park <14cpark@gmail.com>
2018-10-14 06:43:13 +0000

    collect all info into json obj

--- public/js/controllers/singleController.js
+++ public/js/controllers/singleController.js
@@ -3,46 +3,61 @@ angular.module("WebsiteApp").controller("singleController", function($scope, $ht
     function init() {
         console.log("singleController.js init()");
         
-        $scope.schedulecart = [[]];
+        $scope.schedulecart = [];
     }
     
     $scope.addSchedule = function() {
-        var dayselected = angular.element(document.getElementById("day")).val();
+        var dayselected = angular.element(document.getElementById("day")).val(); // raw strings (monday, 14:00, etc)
         var starttimeselected = angular.element(document.getElementById("starttime")).val();
         var endtimeselected = angular.element(document.getElementById("endtime")).val();
         
-        var starttimemins = timeStringToInt(starttimeselected);
-        var endtimemins = timeStringToInt(endtimeselected);
+        var starttimetotalmins = timeStringToInt(starttimeselected); // total number of minutes in int
+        var endtimetotalmins = timeStringToInt(endtimeselected);
+        
         
         console.log("day: ", dayselected);
-        console.log("starttime: ", starttimemins);
-        console.log("endtimeselected: ", endtimemins);
+        console.log("starttime: ", starttimetotalmins);
+        console.log("endtimeselected: ", endtimetotalmins);
         
         if (starttimeselected == "") {
-            console.log("did not enter start time");
+            // console.log("did not enter start time");
             angular.element(document.getElementById("warningtext")).text("Enter start time!");
         }
         else if (endtimeselected == "") {
-            console.log("did not enter end time");
+            // console.log("did not enter end time");
             angular.element(document.getElementById("warningtext")).text("Enter end time!");
         }
-        else if (starttimemins == endtimemins) {
-            console.log("start time can't equal end time");
+        else if (starttimetotalmins == endtimetotalmins) {
+            // console.log("start time can't equal end time");
             angular.element(document.getElementById("warningtext")).text("Start and end time are equal!");
         }
-        else if (starttimemins > endtimemins) {
-            console.log("can't end before you start");
+        else if (starttimetotalmins > endtimetotalmins) {
+            // console.log("can't end before you start");
             angular.element(document.getElementById("warningtext")).text("Can't end before you start.");
         }
         else {
-            console.log("PASS");
+            // console.log("PASS");
             angular.element(document.getElementById("warningtext")).text("Schedule added");
-            var schedarray = []; // array of four numbers
-            $scope.schedulecart.push("");
+            
+            
+            var starttimehr = parseInt(starttimetotalmins / 60); // int values of four numbers
+            var starttimemin = parseInt(starttimetotalmins % 60);
+            var endtimehr = parseInt(endtimetotalmins / 60);
+            var endtimemin = parseInt(endtimetotalmins % 60);
+            
+            var timearray = [starttimehr, starttimemin, endtimehr, endtimemin]; // array of four numbers
+            
+            var text = '{"' + dayselected + '" : [' + timearray + ']}';
+            // console.log("value of text: ", text);
+            var schedule = JSON.parse(text); // JSON object (e.g. {"monday" : [1, 2, 3, 4]} )
+            console.log("value of schedule json obj: ", schedule);
+            
+            $scope.schedulecart.push(schedule);
+            console.log("schedulecart: ", $scope.schedulecart);
         }
     }
-        
-        
+    
+    
     function timeStringToInt(timestr) {
         var hour = timestr.split(":")[0];
         var minute = timestr.split(":")[1];
@@ -50,8 +65,8 @@ angular.module("WebsiteApp").controller("singleController", function($scope, $ht
         return parseInt(hour, 10) * 60 + parseInt(minute, 10);
     }
     
-    function scheduleToTableString() {
-        
+    function scheduleToTableString(sched) {
+        // return 
     }
     
     init();
